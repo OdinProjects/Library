71eb9e8f539b774125aac73c0e317d86e8f1f838
Steven Hans Limantoro <steven.limantoro@gmail.com>
2021-08-02 11:29:44 +0700

    Demonstration with multiple books

--- js/app.js
+++ js/app.js
@@ -3,5 +3,11 @@ import { Book } from "./book.js";
 
 let myLibrary = new Library();
 const newBook = new Book("My CS life");
+const newBook1 = new Book ("Software Engineering 101");
+const newBook2 = new Book ("Serious Cryptography");
+const newBook3 = new Book ("Cryptography Engineering");
 myLibrary.addBookToLibrary(newBook);
+myLibrary.addBookToLibrary(newBook1);
+myLibrary.addBookToLibrary(newBook2);
+myLibrary.addBookToLibrary(newBook3);
 myLibrary.getAllBooks();
